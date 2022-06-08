fb38429b74a378852e870375d45959730de96eb6
Ashay Aswale <42447757+AshayAswale@users.noreply.github.com>
2022-06-08 10:08:37 -0400

    test starts on trigger topic

--- scripts/walk_front_back.py
+++ scripts/walk_front_back.py
@@ -4,15 +4,22 @@ import rospy
 
 from geometry_msgs.msg import Twist
 from geometry_msgs.msg import TwistStamped
+from std_msgs.msg import Bool
 
 import sys, select
 
+start_motion = False
+
+def callback(data):
+    global start_motion
+    start_motion = data.data
+
 TwistMsg = Twist
 
-rospy.init_node('teleop_twist_keyboard')
+rospy.init_node('walk_front_back')
 
 publisher = rospy.Publisher('cmd_vel', TwistMsg, queue_size = 1)
-
+rospy.Subscriber("/start_motion", Bool, callback)
 
 
 velocity_x_set = [0.05,0,-0.05,0]
@@ -35,9 +42,11 @@ i = 0
 # rospy.sleep(2)
 
 while not rospy.is_shutdown():
-    twist.linear.x = velocity_x_set[i]
-    i = i+1 if i<(len(velocity_x_set)-1) else 0
-
+    if not (start_motion):
+        twist.linear.x = 0
+    else:
+        twist.linear.x = velocity_x_set[i]
+        i = i+1 if i<(len(velocity_x_set)-1) else 0
     
     publisher.publish(twist)
     rospy.sleep(walk_timer)
